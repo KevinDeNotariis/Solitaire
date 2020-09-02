37a3700d6291810defbd2890960255f528ca3e42
KevinDeNotariis <68190090+KevinDeNotariis@users.noreply.github.com>
2020-09-02 17:20:35 +0200

    COBOL version

---
+++ COBOL/solitaire.cbl
@@ -0,0 +1,178 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SOLITAIRE.
+       AUTHOR. KEVIN DE NOTARIIS.
+       DATE-WRITTEN. AUGUST 31ST 2020.
+
+       DATA DIVISION.
+       LINKAGE SECTION.
+           
+           01 L-SUITE-TABLE.
+               02 L-SUITES PIC A(6) OCCURS 4 TIMES INDEXED BY I.
+    
+           01 L-DECK.
+               02 L-CARDS OCCURS 40 TIMES INDEXED BY J.
+                   03 L-VALUE PIC 99.
+                   03 L-SUITE PIC A(6).
+
+            01 L-BOQUETS-DECK.
+               02 L-BOQUETS-CARDS OCCURS 40 TIMES INDEXED BY K.
+                   03 L-BOQUETS-VALUE PIC 99.
+                   03 L-BOQUETS-SUITE PIC A(6).        
+
+           01 L-DUMMY-CARD.
+               02 L-DUMMY-VALUE PIC 99.
+               02 L-DUMMY-SUITE PIC A(6).
+       
+           01 L-RANDOM_VALUE_INT PIC 99.
+
+           01 L-PRINT_IND    PIC 99 VALUE 1.
+           01 L-PLAY_IND     PIC 99 VALUE 1.
+           01 L-MOVE_IND     PIC 99 VALUE 1.
+
+           01 L-STAT_BOOL PIC 9 VALUE 0.
+               88 L-PRINT_ALL     VALUE 1.
+               88 L-PRINT_STAT    VALUE 0.
+
+            01 L-STAT-TABLE.
+               02 L-STAT PIC 9(6) OCCURS 40 TIMES INDEXED BY Z.
+
+       PROCEDURE DIVISION USING L-SUITE-TABLE, L-DECK, L-BOQUETS-DECK,
+           L-DUMMY-CARD, L-RANDOM_VALUE_INT, L-PRINT_IND, L-PLAY_IND,
+           L-MOVE_IND, L-STAT_BOOL, L-STAT-TABLE.
+          
+           PERFORM SHUFFLE_DECK
+
+           IF L-STAT_BOOL = 1 THEN
+               PERFORM PRINT_DECK
+           END-IF
+
+           PERFORM PLAY_GAME
+ 
+           ADD 1 TO L-STAT(K)
+
+       EXIT PROGRAM.
+
+       
+       *>******************** Shuffle Deck section *********************
+
+       SHUFFLE_DECK.
+           PERFORM SHUFFLE_DECK_LOOP VARYING J FROM 1 BY 1
+               UNTIL J > 40.
+           
+       SHUFFLE_DECK_LOOP.
+           COMPUTE L-RANDOM_VALUE_INT ROUNDED = (40 * FUNCTION RANDOM)
+           IF L-RANDOM_VALUE_INT = 0 THEN
+               PERFORM SHUFFLE_DECK_LOOP
+           ELSE
+               PERFORM SWAP_ELEMENTS
+           END-IF.
+           
+       SWAP_ELEMENTS.
+
+           MOVE L-CARDS(J) TO L-DUMMY-CARD
+           MOVE L-CARDS(L-RANDOM_VALUE_INT) TO L-CARDS(J)
+           MOVE L-DUMMY-CARD TO L-CARDS(L-RANDOM_VALUE_INT).
+
+       *>***************************************************************
+
+       *>********************* Play Game section ***********************
+
+       PLAY_GAME.
+           SET K TO 0
+           PERFORM PLAY_GAME_LOOP VARYING J FROM 1 BY 1 UNTIL J > 40.
+
+       PLAY_GAME_LOOP.
+           ADD 1 TO K
+           MOVE L-CARDS(J) TO L-BOQUETS-CARDS(K)
+
+           *>Print if required
+           IF L-STAT_BOOL = 1 THEN
+               PERFORM PRINT_BOQUETS
+           END-IF
+
+           PERFORM UNTIL K <= 2
+               IF L-BOQUETS-SUITE(K) = L-BOQUETS-SUITE(K - 2) 
+                OR L-BOQUETS-VALUE(K) = L-BOQUETS-VALUE(K - 2) THEN
+                   MOVE L-BOQUETS-CARDS(K - 1) TO 
+                    L-BOQUETS-CARDS(K - 2)
+                   MOVE L-BOQUETS-CARDS(K) TO L-BOQUETS-CARDS(K - 1)
+                   MOVE "00      " TO L-BOQUETS-CARDS(K)
+
+                   *>Print if required
+                   IF L-STAT_BOOL = 1 THEN
+                       PERFORM PRINT_BOQUETS
+                   END-IF
+
+                   SET K DOWN BY 1
+                   COMPUTE L-PLAY_IND = K - 1
+                   PERFORM PLAY
+               ELSE
+                   EXIT PERFORM
+               END-IF
+           END-PERFORM.
+
+       PLAY.
+           IF L-PLAY_IND > 2 THEN
+               IF L-BOQUETS-SUITE(L-PLAY_IND - 2) = 
+                  L-BOQUETS-SUITE(L-PLAY_IND) OR 
+                  L-BOQUETS-VALUE(L-PLAY_IND - 2) =
+                  L-BOQUETS-VALUE(L-PLAY_IND) THEN
+               
+                   MOVE L-BOQUETS-CARDS(L-PLAY_IND - 1) TO 
+                    L-BOQUETS-CARDS(L-PLAY_IND - 2)
+                   PERFORM MOVE_CARDS VARYING L-MOVE_IND FROM L-PLAY_IND
+                    BY 1 UNTIL L-MOVE_IND > K
+                   MOVE "00      " TO L-BOQUETS-CARDS(K)
+                   
+                   *>Print if required
+                   IF L-STAT_BOOL = 1 THEN
+                       PERFORM PRINT_BOQUETS
+                   END-IF
+
+                   SET K DOWN BY 1
+                   COMPUTE L-PLAY_IND = L-PLAY_IND - 1
+                   PERFORM PLAY
+               END-IF
+           END-IF.
+
+       MOVE_CARDS.
+           MOVE L-BOQUETS-CARDS(L-MOVE_IND) TO 
+            L-BOQUETS-CARDS(L-MOVE_IND - 1).
+       
+       *>***************************************************************
+
+       *>********************* Print Deck section **********************
+       
+       PRINT_DECK.
+           DISPLAY " "
+           DISPLAY "**********************" WITH NO ADVANCING
+           DISPLAY " Deck Configuration "  WITH NO ADVANCING
+           DISPLAY "**********************"
+           PERFORM PRINT_DECK_LOOP VARYING L-PRINT_IND FROM 1 BY 1
+               UNTIL L-PRINT_IND > 40
+           DISPLAY " ".
+
+       PRINT_DECK_LOOP.
+           DISPLAY "(" L-VALUE(L-PRINT_IND) ", " L-SUITE(L-PRINT_IND) 
+           ")" WITH NO ADVANCING.
+           
+       *>***************************************************************
+
+       *>******************* Print Boquets section *********************
+       
+       PRINT_BOQUETS.
+           DISPLAY " "
+      *     DISPLAY "**********************" WITH NO ADVANCING
+      *     DISPLAY " Boquets Configuration "  WITH NO ADVANCING
+      *     DISPLAY "**********************"
+           PERFORM PRINT_BOQUETS_LOOP VARYING L-PRINT_IND FROM 1 BY 1
+               UNTIL L-PRINT_IND > 40
+           DISPLAY " ".
+
+       PRINT_BOQUETS_LOOP.
+           IF L-BOQUETS-CARDS(L-PRINT_IND) NOT EQUAL "00      " THEN
+               DISPLAY "(" L-BOQUETS-VALUE(L-PRINT_IND) 
+               ", " L-BOQUETS-SUITE(L-PRINT_IND) ")" WITH NO ADVANCING
+           END-IF.
+           
+       *>***************************************************************
---
+++ COBOL/solitaire_play_n_games.cbl
@@ -0,0 +1,125 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SOLITAIRE_PLAY_N_GAMES.
+       AUTHOR. KEVIN DE NOTARIIS.
+       DATE-WRITTEN. AUGUST 2ND 2020.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+           01 WS-SUITE-TABLE.
+               02 WS-SUITES PIC A(6) OCCURS 4 TIMES INDEXED BY I.
+    
+           01 WS-DECK.
+               02 WS-CARDS OCCURS 40 TIMES INDEXED BY J.
+                   03 WS-VALUE PIC 99.
+                   03 WS-SUITE PIC A(6).
+
+            01 WS-BOQUETS-DECK.
+               02 WS-BOQUETS-CARDS OCCURS 40 TIMES INDEXED BY K.
+                   03 WS-BOQUETS-VALUE PIC 99.
+                   03 WS-BOQUETS-SUITE PIC A(6).        
+
+           01 VALUE_INDEX  PIC 99 VALUE 1.
+
+           01 WS-DUMMY-CARD.
+               02 WS-DUMMY-VALUE PIC 99.
+               02 WS-DUMMY-SUITE PIC A(6).
+       
+           01 WS-RANDOM_VALUE_INT PIC 99.
+
+           01 WS-PRINT_IND    PIC 99 VALUE 1.
+           01 WS-PLAY_IND     PIC 99 VALUE 1.
+           01 WS-MOVE_IND     PIC 99 VALUE 1.
+
+           01 SEED PIC 9(8) VALUE 11.
+
+           01 WS-NUM_OF_GAMES      PIC 9(6) VALUE 1.
+           01 WS-PLAY_N_GAMES_IND  PIC 9(6) VALUE 1.
+
+           01 WS-STAT_BOOL PIC 9 VALUE 0.
+               88 WS-PRINT_ALL     VALUE 1.
+               88 WS-PRINT_STAT    VALUE 0.
+
+           01 WS-STAT-TABLE.
+               02 WS-STAT PIC 9(6) VALUE 0 OCCURS 40 TIMES INDEXED BY Z.
+
+       PROCEDURE DIVISION.
+
+           *> Init random numbers.
+           COMPUTE WS-RANDOM_VALUE_INT ROUNDED = FUNCTION RANDOM(SEED)
+
+           PERFORM INIT_SUITE_TABLE
+
+           PERFORM INIT_DECK
+
+           DISPLAY "How many games would you like to play? " WITH 
+            NO ADVANCING
+
+           ACCEPT WS-NUM_OF_GAMES
+
+           DISPLAY "Would you like to display all the games (type 1), " 
+            "or just the statistics (type 0)?"
+       
+           ACCEPT WS-STAT_BOOL
+
+           PERFORM PLAY_N_GAMES
+
+           IF WS-STAT_BOOL = 0 THEN
+               PERFORM PRINT_STAT
+           END-IF
+           
+       STOP RUN.
+
+       *>********************* Initialize section **********************
+
+       INIT_SUITE_TABLE.                                       
+          MOVE "CLUBS"     TO WS-SUITES(1)                                     
+          MOVE "SWORDS"    TO WS-SUITES(2)                                     
+          MOVE "COINS"     TO WS-SUITES(3)                                     
+          MOVE "CUPS"      TO WS-SUITES(4).                                        
+                                           
+       INIT_DECK.                                      
+           PERFORM INIT_DECK_SUITE_LOOP VARYING I FROM 1 BY 1                                      
+               UNTIL I > 4.                                        
+                                           
+       INIT_DECK_SUITE_LOOP.                                       
+           PERFORM INIT_DECK_VALUE_LOOP VARYING J FROM 1 BY 1                                      
+               UNTIL J > 10.                                       
+                                               
+       INIT_DECK_VALUE_LOOP.                                       
+           MOVE WS-SUITES(I) TO WS-SUITE(VALUE_INDEX)                                      
+           MOVE J TO WS-VALUE(VALUE_INDEX)                                     
+           ADD 1 TO VALUE_INDEX.                                       
+
+       *>***************************************************************
+       
+       *>******************** Play n games section *********************
+
+       PLAY_N_GAMES.
+           PERFORM PLAY_N_GAMES_LOOP VARYING WS-PLAY_N_GAMES_IND 
+           FROM 1 BY 1 UNTIL WS-PLAY_N_GAMES_IND > WS-NUM_OF_GAMES.
+
+       PLAY_N_GAMES_LOOP.
+           IF WS-STAT_BOOL = 1 THEN
+               DISPLAY " "
+               DISPLAY "----------------------------------------------"
+               DISPLAY "Playing game number: " WS-PLAY_N_GAMES_IND
+               DISPLAY " "
+           END-IF
+           CALL 'SOLITAIRE' USING BY CONTENT WS-SUITE-TABLE, BY CONTENT
+            WS-DECK, BY CONTENT WS-BOQUETS-DECK, 
+            BY CONTENT WS-DUMMY-CARD, BY CONTENT WS-RANDOM_VALUE_INT,
+            BY CONTENT WS-PRINT_IND, BY CONTENT WS-PLAY_IND, BY CONTENT
+            WS-MOVE_IND, WS-STAT_BOOL, BY REFERENCE WS-STAT-TABLE.
+
+       *>***************************************************************
+       
+       *>********************* Print stat section **********************
+       
+       PRINT_STAT.
+           PERFORM PRINT_STAT_LOOP VARYING Z FROM 1 BY 1 UNTIL Z > 40.
+
+       PRINT_STAT_LOOP.
+           DISPLAY Z ": " WS-STAT(Z).
+
+
+       *>***************************************************************
